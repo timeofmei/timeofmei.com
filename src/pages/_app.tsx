cadacc134ef2d0adfb2945718fbd511d636f32c2
timeofmei <timeofmei@outlook.com>
2023-03-09 13:15:54 +0800

    remove useless components

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,4 @@
 import "@/styles/globals.css";
-import "@chinese-fonts/dyh/dist/SmileySans-Oblique/result.css";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,12 +2,12 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="zh">
+    <Html lang="en">
       <Head>
         <meta name="description" content="timeofmei's website" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <body className="bg-cover bg-center bg-background bg-no-repeat">
+      <body className="bg-background bg-cover bg-center">
         <Main />
         <NextScript />
       </body>
