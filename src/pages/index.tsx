e8b17e45846e7c033a3ecded26469bbaf6bead71
timeofmei <timeofmei@outlook.com>
2023-04-18 13:07:27 +0800

    update beian info

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,8 @@
 import Head from "next/head";
 import Image from "next/image";
-import { LinkButton } from "@/components/button";
+import {LinkButton} from "@/components/button";
 import env from "@/share/env";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -10,7 +11,8 @@ export default function Home() {
         <title>{env.websiteTitle}</title>
       </Head>
       <main className="w-screen h-screen flex flex-col justify-center lg:justify-end">
-        <div className="flex flex-col lg:flex-row justify-between items-center py-14 lg:py-0 lg:pb-10 lg:px-12 h-screen lg:h-40 text-white text-2xl">
+        <div
+          className="flex flex-col lg:flex-row justify-between items-center pt-14 lg:py-0 lg:px-12 h-screen lg:h-40 text-white text-2xl">
           <div className="rounded-full overflow-hidden w-40 h-40 relative">
             <Image fill={true} src={"/avatar.webp"} alt={"avatar"} />
           </div>
@@ -18,6 +20,9 @@ export default function Home() {
           <LinkButton text="Blog" url={env.url.blog} />
           <LinkButton text="Projects" url={env.url.projects} />
           <LinkButton text="About" url={env.url.about} />
+        </div>
+        <div className="w-screen flex justify-center text-gray-400">
+          <Link href="https://beian.miit.gov.cn/" target="_blank">闽ICP备2023003034号</Link>
         </div>
       </main>
     </>
